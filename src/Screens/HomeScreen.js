f3a7cf2297230305dc0b80115d5ee4dbff2b87ed
Davy <davyfungaloi@gmail.com>
2021-04-17 22:46:50 +0200

    finished intro container

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,15 +1,22 @@
 import React from "react";
+import coffeeheroimage from "../assets/home/desktop/image-hero-coffeepress.jpg";
 
 const HomeScreen = () => {
   return (
-    <div>
-      <div className="intro-container">
+    <div
+      className="intro-container"
+      style={{ backgroundImage: `url(${coffeeheroimage})` }}
+    >
+      <div className="intro-container__content">
         <h1> Great Coffee made simple.</h1>
-        <div>
+        <div className="intro-container__micro-copy">
           Start your mornings with the world’s best coffees. Try our expertly
           curated artisan coffees from our best roasters delivered directly to
           your door, at your schedule.
         </div>
+        <a href="/subscribe">
+          <button className="btn">Create your plan</button>
+        </a>
       </div>
     </div>
   );
