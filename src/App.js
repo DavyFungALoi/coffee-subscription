bf648db6ba79764bf846888b51f9a317042df189
Davy <davyfungaloi@gmail.com>
2021-04-17 11:21:28 +0200

    added scss

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Footer from "./Components/Footer";
