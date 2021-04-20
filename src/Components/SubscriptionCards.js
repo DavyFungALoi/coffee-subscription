6c81367d8ca7f7aec3e74424bb1a8fba5fb7e6ad
Davy <davyfungaloi@gmail.com>
2021-04-20 16:50:28 +0200

    added the ability to toggle dropdown menu

--- src/Components/SubscriptionCards.js
+++ src/Components/SubscriptionCards.js
@@ -1,26 +1,45 @@
 import React from "react";
+import { useState } from "react";
 import SubscriptionCard from "./SubscriptionCard";
 import arrow from "../assets/plan/desktop/icon-arrow.svg";
 
 const SubscriptionCards = (props) => {
+  const dropDownHandler = () => {
+    setDisplayState((prevDisplayState) => !prevDisplayState);
+  };
+  const [displayState, setDisplayState] = useState(false);
+
   const { question, choices, id, questionId, selectHandler } = props;
   return (
     <div className="question-container">
       <div className="question-container__header">
         <h3 className="question-container__title">{question}</h3>
-        <img className="arrow" src={arrow} alt={"arrow"}></img>
+        <img
+          onClick={() => {
+            dropDownHandler();
+          }}
+          className="arrow"
+          src={arrow}
+          alt={"arrow"}
+        ></img>
       </div>
-      <div className="question-container__choices">
-        {choices.map((choice) => (
-          <SubscriptionCard
-            selectHandler={selectHandler}
-            title={choice.choiceTitle}
-            description={choice.choiceDescription}
-            id={choice.id}
-            key={choice.id}
-            questionId={questionId}
-          ></SubscriptionCard>
-        ))}
+      <div>
+        {displayState ? (
+          <div className="question-container__choices">
+            {choices.map((choice) => (
+              <SubscriptionCard
+                selectHandler={selectHandler}
+                title={choice.choiceTitle}
+                description={choice.choiceDescription}
+                id={choice.id}
+                key={choice.id}
+                questionId={questionId}
+              ></SubscriptionCard>
+            ))}
+          </div>
+        ) : (
+          <div></div>
+        )}
       </div>
     </div>
   );
