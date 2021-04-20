39a3da18fe3297da9fdd3571c049362abdcbf219
Davy <davyfungaloi@gmail.com>
2021-04-20 23:17:45 +0200

    added inline styling for the seperate css classes

--- src/Components/SubscriptionCard.js
+++ src/Components/SubscriptionCard.js
@@ -9,30 +9,20 @@ const SubscriptionCard = (props) => {
     questionId,
     id,
     selectedIdHandler,
-    questionAnswer,
     selectedId,
   } = props;
 
-  const [colorState, setcolorState] = useState("");
-
-  /*useEffect(() => {
-    const currentAnswers = Object.values(questionAnswer);
-    console.log(currentAnswers[0]);
-
-    currentAnswers.forEach((answer) => {
-      if (answer === title) {
-        setcolorState("hello");
-      }
-      if ((currentAnswers[0] = !title)) {
-        setcolorState("kappa");
-        console.log("problem");
-      }
-    });
-  }, [questionAnswer]);
- */
+  const cardSelect = {
+    backgroundColor: "#0E8784",
+    color: "white",
+  };
+  const sameold = {
+    backgroundColor: "#F4F1EB",
+  };
 
   return (
     <div
+      style={selectedId === id ? cardSelect : sameold}
       onClick={() => {
         selectHandler(title, questionId);
         selectedIdHandler(id);
@@ -41,19 +31,8 @@ const SubscriptionCard = (props) => {
     >
       <h4 className="subscription__option-title">{title}</h4>
       <div className="subscription__option-description">{description}</div>
-      <div>{colorState}</div>
-      {selectedId === id ? <div>Hello</div> : <div>false</div>}
     </div>
   );
 };
 
 export default SubscriptionCard;
-/*
-  {colorState ? <div>test</div> : <div>test2</div>}
-   currentAnswers.forEach((key) => {
-      console.log(questionAnswer[key]);
-      if (questionAnswer[key] == title) {
-        console.log(questionAnswer[key]);
-      }
-    });
-*/
