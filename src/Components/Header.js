89fc5ea28cdb722d9cd06d2f3c17b6302f950e8a
Davy <davyfungaloi@gmail.com>
2021-04-21 14:29:02 +0200

    added introduction container css homescreen mobile

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,26 @@
 import React from "react";
 import logo from "../assets/shared/desktop/logo.svg";
+import hamBurgerMenu from "../assets/shared/mobile/icon-hamburger.svg";
+import closeMenu from "../assets/shared/mobile/icon-close.svg";
 
 const Header = () => {
+  const openHamburgerHandler = () => {
+    console.log("hamburger");
+  };
   return (
     <div className="nav-container">
       <a href="/">
         <img className="logo" src={logo} alt={"logo"}></img>
       </a>
+      <div
+        className="nav-container__hamburger"
+        onClick={() => {
+          openHamburgerHandler();
+        }}
+      >
+        <img className="menu" src={hamBurgerMenu} alt={"menu"}></img>
+      </div>
+
       <ul className="nav__list-container">
         <a href="/">
           <li>HOME</li>
